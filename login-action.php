649738c0429da664528a6972e527fa55a66d3126
Nevin <nevin.muhammad@gmail.com>
2021-10-28 15:07:53 +0700

    Tugas web

---
+++ login-action.php
@@ -0,0 +1,13 @@
+<?php
+
+$email = $_POST["email"];
+$password = $_POST["password"];
+
+if($email == "nevin.muhammad@gmail.com" && $pass="12") {
+    echo "Selamat datang";
+}
+else {
+    echo "Password atau email anda salah"
+}
+
+?>
